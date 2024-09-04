848203958a23eeefa353762bec09f07db7573836
Josh <yujong4170@gmail.com>
2024-09-04 11:58:39 +0900

    yujong

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -1,6 +1,11 @@
 import { Module } from '@nestjs/common';
+import { UserService } from './user.service';
+import { UserService } from './user.service';
 
 @Module({
 
+
+
+  providers: [UserService]
 })
 export class UserModule {}
