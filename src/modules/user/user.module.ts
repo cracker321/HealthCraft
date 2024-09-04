62da3a491f77357d963b5db6875b2032ed4018af
Josh <yujong4170@gmail.com>
2024-09-04 11:14:26 +0900

    yujong

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -1,9 +1,6 @@
 import { Module } from '@nestjs/common';
-import { ControllerController } from './controller/controller.controller';
-import { ServiceService } from './service/service.service';
 
 @Module({
 
-  controllers: [ControllerController]
 })
 export class UserModule {}
