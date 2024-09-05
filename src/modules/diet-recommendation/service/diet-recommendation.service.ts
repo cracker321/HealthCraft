8449871dc689b1a69261d919c0b3a45d404a2921
Josh <yujong4170@gmail.com>
2024-09-05 18:19:12 +0900

    yujong

--- src/modules/diet-recommendation/service/diet-recommendation.service.ts
+++ src/modules/diet-recommendation/service/diet-recommendation.service.ts
@@ -5,7 +5,7 @@ import { Repository } from 'typeorm';
 import { Recipe } from '../../nutrition/entity/recipe.entity';
 import { UserService } from '../../user/service/user.service';
 import { NutritionAnalysisService } from '../../nutrition-analysis/service/nutrition-analysis.service';
-import { DietaryRestriction } from '../../nutrition/entity/dietary-restriction.entity';
+import { DietaryRestriction } from '../../dietary-restriction/interfaces/dietary-restriction.interface';
 
 @Injectable()
 export class DietRecommendationService {
@@ -16,12 +16,11 @@ export class DietRecommendationService {
     private nutritionAnalysisService: NutritionAnalysisService
   ) {}
 
-  // 개인화된 식단을 추천하는 메서드
   async recommendPersonalizedDiet(userId: string): Promise<Recipe[]> {
     const user = await this.userService.findOne(userId);
     const nutritionAnalysis = await this.nutritionAnalysisService.analyzeNutritionIntake(
       userId,
-      new Date(Date.now() - 7 * 24 * 60 * 60 * 1000), // 일주일 전
+      new Date(Date.now() - 7 * 24 * 60 * 60 * 1000),
       new Date()
     );
 
@@ -29,11 +28,11 @@ export class DietRecommendationService {
   }
 
   private async findSuitableRecipes(nutritionAnalysis: any, dietaryRestrictions: DietaryRestriction[]): Promise<Recipe[]> {
-    // 영양 분석 결과와 식이 제한을 고려한 레시피 검색 로직
     const restrictionTypes = dietaryRestrictions.map(restriction => restriction.type);
 
     return this.recipeRepository.createQueryBuilder('recipe')
-      .where('recipe.dietaryRestrictions && :restrictions', { restrictions: restrictionTypes })
+      .leftJoinAndSelect('recipe.dietaryRestrictions', 'dietaryRestriction')
+      .where('dietaryRestriction.type IN (:...restrictionTypes)', { restrictionTypes })
       .getMany();
   }
 }
---
+++ src/modules/dietary-restriction/interface/dietary-restriction.interface.ts
@@ -0,0 +1,7 @@
+// src/modules/dietary-restriction/interfaces/dietary-restriction.interface.ts
+export interface DietaryRestriction {
+    id: string;
+    type: string;
+    name: string;
+    description?: string;
+  }
